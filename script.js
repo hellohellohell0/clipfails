2326a5d25b758c44603b81dff6b13553b2eb46e5
hellohellohell0 <hhellohellohell012322@gmail.com>
2025-09-05 23:05:38 -0700

    Update script.js

--- script.js
+++ script.js
@@ -352,8 +352,8 @@ async function loadClips() {
             // 1689 309
             document.getElementById("featuredMeta").innerHTML = `
                 <span class="platform-badge ${featured.platform}">${featured.platform.toUpperCase()}</span>
-                <span>👀 982 views</span> 
-                <span>❤️ 102 likes</span>
+                <span>👀 1689 views</span> 
+                <span>❤️ 309 likes</span>
             `;
         }
 
