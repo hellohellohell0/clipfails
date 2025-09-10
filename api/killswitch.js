0c31a095a81e1a78e29a9aeeae81f1417b6fd81f
hellohellohell0 <hhellohellohell012322@gmail.com>
2025-09-09 17:41:50 -0700

    Create killswitch.js

---
+++ api/killswitch.js
@@ -0,0 +1,12 @@
+export default async function handler(req, res) {
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    try {
+        const response = await fetch("https://4b646c79-7d89-477b-af11-791e5a27cd07-00-1pxopkbkgfwrk.worf.replit.dev?type=getredirect");
+        if (!response.ok) throw new Error("Bad response");
+
+        const text = await response.text();
+        res.status(200).send(text.trim() || "none");
+    } catch (err) {
+        res.status(200).send("none");
+    }
+}
